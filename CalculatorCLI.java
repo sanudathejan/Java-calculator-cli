41a8082220279b71fd827664f9946ba02e755180
sanudathejan <thejansanuda2006@gmail.com>
2025-08-19 21:07:29 +0530

    Update CalculatorCLI.java

---
+++ CalculatorCLI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CalculatorCLI {
+    public void main(String [] args) {
+        Scanner scanner = new Scanner)System.in);
+        boolean running = true :
+
+        System.out.println("Simple Calculator ");
+        System.out.println("Available operations in project are : + - * / ");
+        System.out.println("Type 'exit' to quit\n");
+
+        while (running) {
+            
+        }
+
+
+
+    }
+}
