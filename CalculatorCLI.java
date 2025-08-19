1468fa7456adf71bff2e9cfb54c8b34e8124999d
sanudathejan <thejansanuda2006@gmail.com>
2025-08-19 23:01:09 +0530

    Update CalculatorCLI.java

--- CalculatorCLI.java
+++ CalculatorCLI.java
@@ -9,11 +9,65 @@ public class CalculatorCLI {
         System.out.println("Available operations in project are : + - * / ");
         System.out.println("Type 'exit' to quit\n");
 
-        while (running) {
-            
-        }
+        while (running) { 
+            System.out.println("Enter first number: ");
+            String input = scanner.next();
 
+            if (input.equalsIgnoreCase("exit")) {
+                running = false;
+                break;
+            }
+             double num1;
+             try{ 
+                num1 = Double.parseDouble(input);
+             }catch (NumberFormatException e) {
+                System.out.println("Invalid Number! Try again.\n");
+                continue;
 
+             }  
+             System.out.print("Enter operator (+ - * /): ");
+             char operator = scanner.next().charAt(0);
+             
+             System.out.print("Enter second number:");
+             double num2;
+             try { 
+                num2 = Double.parseDouble(scanner.next());
+             } catch (NumberFormatException e ) {
+                System.out.println("Invalid number! Try again.\n");
+                continue;
+             }
 
+             double result = 0;
+             boolean validOperation = true;
+
+             switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("Error!! Division by zero.");
+                        validOperation = false;
+                    } else {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operator!");
+                    validOperation = false;
+             }
+             if (valiOperation) {
+                System.out.println("Result: "+ result + "\n");
+             } 
+        } 
+
+        System.out.println("Calculator exited.");
+        scanner.close();
     }
 }
